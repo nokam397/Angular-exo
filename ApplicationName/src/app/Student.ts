d062576e2a6025c26e8f93899aab0df22ee6eb14
nokam397 <117632934+nokam397@users.noreply.github.com>
2022-11-07 16:36:04 +0100

    exo 1

---
+++ ApplicationName/src/app/Student.ts
@@ -0,0 +1,11 @@
+export class Student {
+ 
+    id: number;
+    name: string;
+    sexe: string;
+    joinDate: Date;
+    age: number;
+    picture: string;
+
+
+}
--- ApplicationName/src/app/app.component.ts
+++ ApplicationName/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+import { STUDENTS } from './mock-student-list';
+import { Student } from './Student';
+
 
 @Component({
   selector: 'app-root',
   template: `
     
       <h1>
-        Welcome to {{title}}!
+        bonjour Gael!
       </h1>
     
   `,
   styles: []
 })
-export class AppComponent {
-  title = 'ApplicationName';
+
+export class AppComponent implements OnInit{
+
+  StudentList: Student[] = STUDENTS;
+
+  ngOnInit(): void{
+
+    console.table(this.StudentList);
+    console.log(this.StudentList[0].name)
+  }
+
+  selectStudent = (studentName: string)=> {
+    console.log(`vous avez cliqué sur l'etudiant ${studentName}`)
+  }
 }
+
